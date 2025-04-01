0232c104dfcc57a5e27e9f917d72a4b1a0f1a454
eugenetan01 <eugenetanweihong@gmail.com>
2025-04-01 14:12:17 +0800

    change basic auth creds

--- test-cases/07.1-Salesforce_Login.sh
+++ test-cases/07.1-Salesforce_Login.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 curl --request POST \
   --url 'http://localhost:8000/services/oauth2/token?grant_type=password&client_id=3MVG9iLRabl2Tf4iJPej_fg3E31d_TEB3EvTIFIYZ4dRB5nT6POJhpL8Is12GQifxpH.s.mNkSmPZE0GK5pcA&client_secret=C3E0F22EE24D87691A574D0D74368962B4968F6AB9AECB2634D44BA0A59A219A&username=integration%40mintel.tech.newtg.tgsandbox&password=Salesforce01ucAj1KKW7O4jpd30dcthVVlk' \
-  --header 'Authorization: Basic ZGVtb3VzZXI6cGFzc3dvcmQ='
+  --header 'Authorization: Basic aW50ZWdyYXRpb24lNDBtaW50ZWwudGVjaC5uZXd0Zy50Z3NhbmRib3g6U2FsZXNmb3JjZTAxdWNBajFLS1c3TzRqcGQzMGRjdGhWVmxr'
