228af866f76888d3caea8dfcb4f84930a262f079
eugenetan01 <eugenetanweihong@gmail.com>
2025-04-01 12:38:49 +0800

    update certs

---
+++ test-cases/04.1-MyIDTravelPayment-nocert.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+curl https://localhost:8443/MyIdTravel/PaymentInterfaceService \
+  -H "Content-Type: application/xml" \
+  -H "User-Agent: sample-client/1.0" \
+  --data-binary @- <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.payment.myidtravel.lhsystems.com">
+  <soapenv:Header/>
+  <soapenv:Body>
+    <ser:authorizePaymentRequestSchema>
+      <ser:Airline>TG</ser:Airline>
+      <ser:PaymentProcessId>000000</ser:PaymentProcessId>
+      <ser:ExtPaymentProcessId></ser:ExtPaymentProcessId>
+      <ser:User>TgLHmid1</ser:User>
+      <ser:Password>cHecepHu?2VV</ser:Password>
+      <ser:Amount DecimalPlaces="2" ValueFormat="#0.00">
+        <ser:NUCValue>0.0</ser:NUCValue>
+        <ser:Currency>USD</ser:Currency>
+        <ser:Value>0.01</ser:Value>
+        <ser:PreferredValue>0.0</ser:PreferredValue>
+        <ser:BSR>0.0</ser:BSR>
+      </ser:Amount>
+      <ser:Creditcard>
+        <ser:CCNumber>4111111111111111</ser:CCNumber>
+        <ser:CCValidCode>123</ser:CCValidCode>
+        <ser:CCVendor>VI</ser:CCVendor>
+        <ser:CCValidity>1024</ser:CCValidity>
+        <ser:CCName>ABCD</ser:CCName>
+        <ser:FOP>CREDIT_CARD</ser:FOP>
+        <ser:PaymentProcessId>000000</ser:PaymentProcessId>
+        <ser:isPaidViaPSP>false</ser:isPaidViaPSP>
+        <ser:createTSTwithoutPaymentInfo>false</ser:createTSTwithoutPaymentInfo>
+        <ser:UATPTamara>false</ser:UATPTamara>
+      </ser:Creditcard>
+      <ser:DoSettlement>true</ser:DoSettlement>
+      <ser:Pnr>?</ser:Pnr>
+    </ser:authorizePaymentRequestSchema>
+  </soapenv:Body>
+</soapenv:Envelope>
+EOF
